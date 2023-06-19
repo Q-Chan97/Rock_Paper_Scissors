9c4af9db78d0493b92a00bc36da5ae33d26c52b1
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2023-06-19 12:09:37 +0900

    Create playRound test function

--- js/script.js
+++ js/script.js
@@ -3,3 +3,19 @@ function getComputerChoice() {
    let index = Math.floor(Math.random() * 3);
    return moves[index];
 }
+
+function playRound(playerSelection, computerSelection) {
+   if (computerSelection === playerSelection) {
+      return "Aww, it's a tie";
+   }
+   else if (computerSelection === "paper" && playerSelection === "rock") {
+      return "You lose! Paper beats rock";
+   }
+   else if (computerSelection === "scissors" && playerSelection === "rock") {
+      return "You win! Rock beats scissors";
+   }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection))
